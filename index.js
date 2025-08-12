3e13b9fb62d84fc0189055c0ac66111f486946c5
Gustavo869 <162450568+Gustavo869@users.noreply.github.com>
2025-08-12 12:15:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,20 @@ app.post("/usuarios", (req, res)=>{
 app.get("/usuarios", (req, res)=>{
     res.send(usuarios)
 })
+
+const produtos = []
+
+app.post("/produtos", (req, res)=>{
+    const produto = req.body
+    produtos.push(produto)
+    res.send("Compra realizada")
+})
+
+app.get("/produtos", (req, res)=>{
+    res.send(produtos)
+})
+
+
 app.listen(port, () =>{
     console.log("API RODANDO NA PORTA " + port)
 })
