bda400a25a639b975b603b0248cde7a859525d50
Gustavo869 <162450568+Gustavo869@users.noreply.github.com>
2025-09-02 10:55:47 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,42 +1,22 @@
+// npm init
+// npm i express
 const express = require("express")
 const app = express()
-const  port = 3000
+const port = 3000
 app.use(express.json())
 
-app.get("/bem vindo", (req, res)=>{
-    res.send("Óla, seja bem vindo a API")
+// npm i mysql2
+const db = require("./db")
+
+app.get("/clientes", async (req,res) => {
+    try {
+        const resultado = await db.pool.query("SELECT * FROM cliente")
+        res.status(200).json(resultado[0])
+    } catch (erro){
+        console.log(erro)
+    }
 })
 
-app.post("/bem vindo", (req, res)=>{
-    const dados = req.body           
-    res.send("Óla, seja bem vindo" + dados.nome)
-})
-
-
-const usuarios = []
-app.post("/usuarios", (req, res)=>{
-    const usuario = req.body
-    usuarios.push(usuario)
-    res.send("Usuario cadastrado com sucesso!")
-})
-
-app.get("/usuarios", (req, res)=>{
-    res.send(usuarios)
-})
-
-const produtos = []
-
-app.post("/produtos", (req, res)=>{
-    const produto = req.body
-    produtos.push(produto)
-    res.send("Compra realizada")
-})
-
-app.get("/produtos", (req, res)=>{
-    res.send(produtos)
-})
-
-
-app.listen(port, () =>{
-    console.log("API RODANDO NA PORTA " + port)
+app.listen(port, ()=>{
+    console.log("API executando na porta " + port)
 })
